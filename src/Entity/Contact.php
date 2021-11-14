21a058ffff4164afd8ce55e35c4c859bd0a764f5
Giom <labaouici02@gmail.com>
2021-11-14 14:42:10 +0100

    Amélioration du contact.php
    Ajout des contraintes pour le nom/prénom/téléphone/mail/age

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -23,32 +23,64 @@ class Contact
 
     /**
      * @ORM\Column(type="string", length=50)
+     * @Assert\Length(
+     *     min = 3
+     *     max = 50
+     *     minMessage = "Votre nom doit être d'au moins 3 lettres"
+     *     maxMessage = "Votre nom doit faire au maximum {{ limit }}"
+     * )
      */
     private $Nom;
 
     /**
      * @ORM\Column(type="string", length=50)
+     * @Assert\Length(
+     *     min = 3
+     *     max = 50
+     *     minMessage = "Votre prénom doit être d'au moins 3 lettres"
+     *     maxMessage = "Votre prénom doit faire au maximum {{ limit }}"
+     * )
      */
     private $Prenom;
 
     /**
      * @ORM\Column(type="string", length=50)
      * @Assert\Email
+     *     @Assert\Length(
+     *     max = 50
+     *     maxMessage = "Votre e-mail doit faire au maximum {{ limit }}"
+     * )
      */
     private $Email;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(
+     *     max = 255
+     *     maxMessage = "Votre adresse doit faire au maximum {{ limit }}"
+     * )
      */
     private $Adresse;
 
     /**
      * @ORM\Column(type="string", length=15)
+     *     @Assert\Length(
+     *     min = 8
+     *     max = 15
+     *     minMessage = "Votre numéro de téléphone doit être d'au moins 8 chiffres"
+     *     maxMessage = "Votre numéro de téléphone doit faire au maximum {{ limit }}"
+     * )
      */
     private $Telephone;
 
     /**
      * @ORM\Column(type="integer", length=3)
+     *      * @Assert\Length(
+     *     min = 2
+     *     max = 3
+     *     minMessage = "Votre âge doit être d'au moins 2 chiffres"
+     *     maxMessage = "Votre âge doit faire au maximum {{ limit }}"
+     * )
      */
     private $Age;
 
